aae638a71adadecc50f5c15bc37649d3dcf95d97
unknown <leodelmiroms@gmail.com>
2021-03-19 14:09:46 -0300

    Autenticacao e autorizacao

---
+++ src/main/java/com/leodelmiro/transacao/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.leodelmiro.transacao.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                authorizeRequests.anyRequest().authenticated()
+        )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+
+
+}
